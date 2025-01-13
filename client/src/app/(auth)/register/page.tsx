25195eb5c9525499a134898d0e67bd56c07d7ab3
nanoha <nanoha.creator@gmail.com>
2025-01-13 21:54:41 +0900

    アカウント登録ページカスタマイズ

--- client/src/app/(auth)/register/page.tsx
+++ client/src/app/(auth)/register/page.tsx
@@ -1,12 +1,24 @@
 'use client';
 
 import Button from '@/components/Button';
-import Input from '@/components/Input';
-import InputError from '@/components/InputError';
-import Label from '@/components/Label';
 import Link from 'next/link';
 import { useAuth } from '@/hooks/auth';
-import { useState } from 'react';
+import React from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { FormItem } from '@/components';
+
+interface FormInputs {
+    name: string;
+    email: string;
+    password: string;
+    passwordConfirmation: string;
+}
+
+type visibleErrorFields =
+    | 'name'
+    | 'email'
+    | 'password'
+    | 'passwordConfirmation';
 
 const Page = () => {
     const { register } = useAuth({
@@ -14,107 +26,238 @@ const Page = () => {
         redirectIfAuthenticated: '/dashboard',
     });
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordConfirmation, setPasswordConfirmation] = useState('');
-    const [errors, setErrors] = useState<Record<string, string[]>>({});
+    const {
+        handleSubmit,
+        control,
+        formState: { errors },
+    } = useForm<FormInputs>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: '',
+            passwordConfirmation: '',
+        },
+    });
 
-    const submitForm = event => {
-        event.preventDefault();
+    const [apiErrors, setApiErrors] = React.useState<Record<string, string[]>>(
+        {},
+    );
 
+    // 入力エラーがあったとき、入力内容の変更でエラーを非表示にする
+    const [isErrorVisible, setIsErrorVisible] = React.useState<
+        Record<visibleErrorFields, boolean>
+    >({
+        name: false,
+        email: false,
+        password: false,
+        passwordConfirmation: false,
+    });
+
+    const onSubmit: SubmitHandler<FormInputs> = (data: FormInputs) => {
         register({
-            name,
-            email,
-            password,
-            password_confirmation: passwordConfirmation,
-            setErrors,
+            name: data.name,
+            email: data.email,
+            password: data.password,
+            password_confirmation: data.passwordConfirmation,
+            setErrors: setApiErrors,
+        });
+        setIsErrorVisible({
+            name: true,
+            email: true,
+            password: true,
+            passwordConfirmation: true,
         });
     };
 
     return (
-        <form onSubmit={submitForm}>
-            {/* Name */}
-            <div>
-                <Label htmlFor="name">Name</Label>
+        <>
+            <div className="flex flex-col gap-y-10">
+                <div className="relative w-full text-center">
+                    <span className="absolute left-0 top-1/2 -translate-y-1/2 w-full h-px bg-gray-main" />
+                    <h1 className="relative w-fit mx-auto px-4 bg-white">
+                        アカウント登録
+                    </h1>
+                </div>
+                <p>
+                    {/* TODO: リンク設定 */}
+                    <Link
+                        href="#"
+                        className="text-base text-primary-main underline transition-opacity hover:text-opacity-70">
+                        利用規約
+                    </Link>
+                    と{/* TODO: リンク設定 */}
+                    <Link
+                        href="#"
+                        className="text-base text-primary-main underline transition-opacity hover:text-opacity-70">
+                        プライバシーポリシー
+                    </Link>
+                    に同意の上、ご登録ください
+                </p>
+                <form
+                    onSubmit={handleSubmit(onSubmit)}
+                    className="flex flex-col gap-y-10">
+                    <div className="flex flex-col gap-y-4">
+                        {/* Name */}
+                        <FormItem
+                            label="ユーザ名"
+                            errorMessage={
+                                isErrorVisible
+                                    ? [
+                                          errors.name?.message,
+                                          ...(apiErrors?.name || []),
+                                      ]
+                                    : []
+                            }>
+                            <Controller
+                                control={control}
+                                name="name"
+                                rules={{
+                                    required: '必須項目です',
+                                }}
+                                render={({ field: { onChange, value } }) => (
+                                    <input
+                                        type="text"
+                                        value={value}
+                                        onChange={e => {
+                                            onChange(e);
+                                            setIsErrorVisible(prev => ({
+                                                ...prev,
+                                                name: false,
+                                            }));
+                                        }}
+                                        className={`py-2 px-4 text-base border rounded-lg ${isErrorVisible.name && (errors.name || apiErrors.name) ? 'border-alert-main' : 'border-gray-main'}`}
+                                    />
+                                )}
+                            />
+                        </FormItem>
 
-                <Input
-                    id="name"
-                    type="text"
-                    value={name}
-                    className="block mt-1 w-full"
-                    onChange={event => setName(event.target.value)}
-                    required
-                    autoFocus
-                />
+                        {/* Email Address */}
+                        <FormItem
+                            label="メールアドレス"
+                            errorMessage={
+                                isErrorVisible
+                                    ? [
+                                          errors.email?.message,
+                                          ...(apiErrors?.email || []),
+                                      ]
+                                    : []
+                            }>
+                            <Controller
+                                control={control}
+                                name="email"
+                                rules={{
+                                    required: '必須項目です',
+                                    pattern: {
+                                        value: /^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/,
+                                        message:
+                                            'メールアドレスの形式で入力してください',
+                                    },
+                                }}
+                                render={({ field: { onChange, value } }) => (
+                                    <input
+                                        type="email"
+                                        value={value}
+                                        onChange={e => {
+                                            onChange(e);
+                                            setIsErrorVisible(prev => ({
+                                                ...prev,
+                                                email: false,
+                                            }));
+                                        }}
+                                        className={`py-2 px-4 text-base border rounded-lg ${isErrorVisible.email && (errors.email?.message || apiErrors.email) ? 'border-alert-main' : 'border-gray-main'}`}
+                                    />
+                                )}
+                            />
+                        </FormItem>
 
-                <InputError messages={errors.name} className="mt-2" />
+                        {/* Password */}
+                        <FormItem
+                            label="パスワード"
+                            errorMessage={
+                                isErrorVisible
+                                    ? [
+                                          errors.password?.message,
+                                          ...(apiErrors?.password || []),
+                                      ]
+                                    : []
+                            }>
+                            <Controller
+                                control={control}
+                                name="password"
+                                rules={{ required: '必須項目です' }}
+                                render={({ field: { onChange, value } }) => (
+                                    <input
+                                        type="password"
+                                        value={value}
+                                        onChange={e => {
+                                            onChange(e);
+                                            setIsErrorVisible(prev => ({
+                                                ...prev,
+                                                password: false,
+                                            }));
+                                        }}
+                                        className={`py-2 px-4 text-base border rounded-lg ${isErrorVisible.password && (errors.password?.message || apiErrors.password) ? 'border-alert-main' : 'border-gray-main'}`}
+                                    />
+                                )}
+                            />
+                        </FormItem>
+
+                        {/* Confirm Password */}
+                        <FormItem
+                            label="パスワード（確認用）"
+                            errorMessage={
+                                isErrorVisible
+                                    ? [
+                                          errors.passwordConfirmation?.message,
+                                          ...(apiErrors?.passwordConfirmation ||
+                                              []),
+                                      ]
+                                    : []
+                            }>
+                            <Controller
+                                control={control}
+                                name="passwordConfirmation"
+                                rules={{ required: '必須項目です' }}
+                                render={({ field: { onChange, value } }) => (
+                                    <input
+                                        type="password"
+                                        value={value}
+                                        onChange={e => {
+                                            onChange(e);
+                                            setIsErrorVisible(prev => ({
+                                                ...prev,
+                                                passwordConfirmation: false,
+                                            }));
+                                        }}
+                                        className={`py-2 px-4 text-base border rounded-lg ${isErrorVisible.passwordConfirmation && (errors.passwordConfirmation?.message || apiErrors.passwordConfirmation) ? 'border-alert-main' : 'border-gray-main'}`}
+                                    />
+                                )}
+                            />
+                        </FormItem>
+                    </div>
+                    <Button>アカウント登録</Button>
+                </form>
+                <div className="flex flex-col items-center gap-y-4">
+                    <Link
+                        href="/login"
+                        className="text-base font-bold text-primary-main underline transition-opacity hover:text-opacity-70">
+                        ログインはこちら
+                    </Link>
+                </div>
             </div>
-
-            {/* Email Address */}
-            <div className="mt-4">
-                <Label htmlFor="email">Email</Label>
-
-                <Input
-                    id="email"
-                    type="email"
-                    value={email}
-                    className="block mt-1 w-full"
-                    onChange={event => setEmail(event.target.value)}
-                    required
-                />
-
-                <InputError messages={errors.email} className="mt-2" />
+            <div className="flex flex-col gap-y-10">
+                <div className="relative w-full text-center">
+                    <span className="absolute left-0 top-1/2 -translate-y-1/2 w-full h-px bg-gray-main" />
+                    <h1 className="relative w-fit mx-auto px-4 bg-white">
+                        他の方法でログイン
+                    </h1>
+                </div>
+                {/* TODO: リンク？ */}
+                <Button type="button" variant="outlined" colorVariant="gray">
+                    Googleアカウントでログイン
+                </Button>
             </div>
-
-            {/* Password */}
-            <div className="mt-4">
-                <Label htmlFor="password">Password</Label>
-
-                <Input
-                    id="password"
-                    type="password"
-                    value={password}
-                    className="block mt-1 w-full"
-                    onChange={event => setPassword(event.target.value)}
-                    required
-                    autoComplete="new-password"
-                />
-
-                <InputError messages={errors.password} className="mt-2" />
-            </div>
-
-            {/* Confirm Password */}
-            <div className="mt-4">
-                <Label htmlFor="passwordConfirmation">Confirm Password</Label>
-
-                <Input
-                    id="passwordConfirmation"
-                    type="password"
-                    value={passwordConfirmation}
-                    className="block mt-1 w-full"
-                    onChange={event =>
-                        setPasswordConfirmation(event.target.value)
-                    }
-                    required
-                />
-
-                <InputError
-                    messages={errors.password_confirmation}
-                    className="mt-2"
-                />
-            </div>
-
-            <div className="flex items-center justify-end mt-4">
-                <Link
-                    href="/login"
-                    className="underline text-sm text-gray-600 hover:text-gray-900">
-                    Already registered?
-                </Link>
-
-                <Button className="ml-4">Register</Button>
-            </div>
-        </form>
+        </>
     );
 };
 
