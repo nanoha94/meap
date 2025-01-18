ff2f1df2704855b6ca872dddcd2056f44dc8ffa0
nanoha <nanoha.creator@gmail.com>
2025-01-18 11:56:28 +0900

    仮登録完了ページをカスタマイズ

--- client/src/app/(auth)/email/verify/page.tsx
+++ client/src/app/(auth)/email/verify/page.tsx
@@ -1,11 +1,10 @@
 'use client';
-
 import Button from '@/components/Button';
 import { useAuth } from '@/hooks/auth';
 import { useState } from 'react';
 
 const Page = () => {
-    const { logout, resendEmailVerification } = useAuth({
+    const { resendEmailVerification } = useAuth({
         middleware: 'auth',
         redirectIfAuthenticated: '/dashboard',
     });
@@ -14,31 +13,41 @@ const Page = () => {
 
     return (
         <>
-            <div className="mb-4 text-sm text-gray-600">
-                Thanks for signing up! Before getting started, could you verify
-                your email address by clicking on the link we just emailed to
-                you? If you didn't receive the email, we will gladly send you
-                another.
-            </div>
-
-            {status === 'verification-link-sent' && (
-                <div className="mb-4 text-sm text-green-600">
-                    A new verification link has been sent to the email address
-                    you provided during registration.
+            <div className="flex flex-col gap-y-10">
+                <div className="relative w-full text-center">
+                    <span className="absolute left-0 top-1/2 -translate-y-1/2 w-full h-px bg-gray-main" />
+                    <h1 className="relative w-fit mx-auto px-4 bg-white">
+                        仮登録完了
+                    </h1>
                 </div>
-            )}
+                <div className="flex flex-col gap-y-4">
+                    <p className="text-xl font-bold">
+                        メールアドレス認証をお願いします
+                    </p>
+                    <div className="flex flex-col gap-y-2">
+                        <p>
+                            ご登録ありがとうございます！現在はまだ仮登録の状態です。
+                        </p>
+                        <p>
+                            info@meap.comよりメールを送信しましたので、メール本文に記載のあるリンクをクリックして認証を完了してください。
+                        </p>
+                    </div>
+                </div>
 
-            <div className="mt-4 flex items-center justify-between">
-                <Button onClick={() => resendEmailVerification({ setStatus })}>
-                    Resend Verification Email
-                </Button>
-
-                <button
-                    type="button"
-                    className="underline text-sm text-gray-600 hover:text-gray-900"
-                    onClick={logout}>
-                    Logout
-                </button>
+                <div className="flex flex-col gap-y-4">
+                    <p>
+                        メールが届かない場合は以下のボタンをクリックして再送してください。
+                    </p>
+                    <Button
+                        onClick={() => resendEmailVerification({ setStatus })}>
+                        認証メールを再送する
+                    </Button>
+                    {status === 'verification-link-sent' && (
+                        <p className="text-alert-main">
+                            登録時に入力されたメールアドレス宛にメールアドレス確認リンクを再送しました
+                        </p>
+                    )}
+                </div>
             </div>
         </>
     );
