bf51dd2888255eb0fd31c99b260d5a9dd68d361c
nanoha <nanoha.creator@gmail.com>
2025-03-07 19:40:29 +0900

    キャッシュクリア

--- server/pre-deploy.sh
+++ server/pre-deploy.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# マイグレーションのリセット
-echo "Running migrate:reset..."
-php artisan migrate:reset || { echo "migrate:reset failed"; exit 1; }
-
-# マイグレーションの実行
-echo "Running migrate..."
-php artisan migrate || { echo "migrate failed"; exit 1; }
-
-# 成功した場合にメッセージを表示
-echo "Migration completed successfully."
+php artisan config:clear
+php artisan cache:clear
+php artisan config:cache
+php artisan migrate --force
