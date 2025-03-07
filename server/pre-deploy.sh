fc1b4551040695f1f180093cfb61032e02036cda
nanoha <nanoha.creator@gmail.com>
2025-03-07 20:38:39 +0900

    Dockerfile更新

--- server/pre-deploy.sh
+++ server/pre-deploy.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-echo "Pre Deploy Start..."
-php artisan config:clear
-php artisan cache:clear
-php artisan config:cache
-php artisan tinker --execute="dump(config('database.connections.mysql'))"
 echo "Migration Start..."
-php artisan migrate:reset
-php artisan migrate
+# php artisan migrate:reset
+# php artisan migrate
 echo "Migration End"
-echo "Pre Deploy End"
