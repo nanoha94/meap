0b837e191e0fe5df807a127b879f895c00942e45
nanoha <nanoha.creator@gmail.com>
2025-03-07 19:56:00 +0900

    デプロイ前の処理を変更

--- server/pre-deploy.sh
+++ server/pre-deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo "Pre Deploy Start..."
 php artisan config:clear
 php artisan cache:clear
 php artisan config:cache
-php artisan migrate --force
+php artisan tinker --execute="dump(config('database.connections.mysql'))"
+echo "...Pre Deploy End"
