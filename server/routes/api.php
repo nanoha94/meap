dcaf6b9990090db956b4f710ba02754269210fc3
nanoha <nanoha.creator@gmail.com>
2025-05-07 08:52:23 +0900

    必要なAPIのコントローラファイルを作成し、ルーティング設定まで

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,23 +1,31 @@
 <?php
 
+use App\Http\Controllers\Api\DishCategoryController;
+use App\Http\Controllers\Api\DishController;
 use App\Http\Controllers\Api\GroupUsersController as ApiGroupUsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\InvitationController;
 use App\Http\Controllers\Api\ShoppingCategoryController;
 use App\Http\Controllers\Api\ShoppingItemController;
+use App\Http\Controllers\Api\MealController;
+use App\Http\Controllers\Api\PageShoppingListController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::middleware('auth')->group(function () {
+    // resource
+    Route::apiResource('/meals', MealController::class);
+    Route::apiResource('/dishes', DishController::class);
+    Route::apiResource('/dishes/caategories', DishCategoryController::class)->except(['show']);
     Route::resource('invitations', InvitationController::class)->only(['store', 'show']);
     Route::post('/invitations/{token}/join', [InvitationController::class, 'join']);
-
     Route::get('/users', [ApiGroupUsersController::class, 'index']);
-
     Route::apiResource('/shopping/items', ShoppingItemController::class)->except(['show']);
-
     Route::apiResource('/shopping/categories', ShoppingCategoryController::class)->except(['show']);
+
+    // page
+    Route::get('/page/shopping-list', PageShoppingListController::class);
 });
