f2924244884f93a733578fdb7d3c81fb45f22f59
nanoha <nanoha.creator@gmail.com>
2025-05-31 11:23:12 +0900

    WIP：API設計

--- server/app/Swagger/MasterResponses.php
+++ server/app/Swagger/MasterResponses.php
@@ -14,6 +14,18 @@ namespace App\Swagger;
  *             description="料理カテゴリ一覧",
  *             @OA\Items(ref="#/components/schemas/DishCategory")
  *         ),
+ * 　　　　@OA\Property(
+ *             property="shoppingCategories",
+ *             type="array",
+ *             description="買い物カテゴリ一覧",
+ *             @OA\Items(ref="#/components/schemas/ShoppingCategory")
+ *         ),
+ *         @OA\Property(
+ *             property="shoppingTags",
+ *             type="array",
+ *             description="買い物タグ一覧",
+ *             @OA\Items(ref="#/components/schemas/ShoppingTag")
+ *         ),
  *     )
  * )
  */
