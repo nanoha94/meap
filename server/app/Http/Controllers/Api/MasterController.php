b1f654b0fdc032a2b92fedd56363c310a4225f85
nanoha <nanoha.creator@gmail.com>
2025-06-08 20:20:15 +0900

    マスターデータ取得APIを実装

--- server/app/Http/Controllers/Api/MasterController.php
+++ server/app/Http/Controllers/Api/MasterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class MasterController extends Controller
@@ -18,8 +19,19 @@ class MasterController extends Controller
      *     @OA\Response(response=404, ref="#/components/responses/NotFound")
      * )
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): JsonResponse
     {
-        //
+        $user = $request->user();
+        $group = $user->group;
+
+        $shopping_ategories = $group->shoppingCategories()->select('id', 'name')->get();
+        $shopping_tags = $group->shoppingTags()->select('id', 'name')->get();
+        $res = [
+            'dishCategories' => [],
+            'shoppingCategories' => $shopping_ategories,
+            'shoppingTags' => $shopping_tags,
+        ];
+
+        return response()->json($res, 200);
     }
 }
