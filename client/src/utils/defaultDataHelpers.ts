0b4f60a54b5d2ca35a68d2b976b7463dcdaba679
nanoha <nanoha.creator@gmail.com>
2025-11-28 08:10:52 +0900

    同じidが生成されてしまうので修正

--- client/src/utils/defaultDataHelpers.ts
+++ client/src/utils/defaultDataHelpers.ts
@@ -1,4 +1,10 @@
 /**
+ * 一時的なIDを生成するためのカウンター
+ * 同じタイミングで呼ばれた場合でも一意なIDを生成するため
+ */
+let idCounter = 0;
+
+/**
  * 一時的なIDを生成してオブジェクトに追加する
  * @param defaultItem デフォルトのオブジェクト
  * @param prefix IDのプレフィックス
@@ -8,7 +14,7 @@ export function createDefaultData<T extends { id?: string }>(
     defaultItem: T,
     prefix: string,
 ): T & { id: string } {
-    const id = `${prefix}${Date.now()}`;
+    const id = `${prefix}${++idCounter}-${Date.now()}`;
     return {
         ...defaultItem,
         id,
