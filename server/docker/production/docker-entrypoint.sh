6a19fdeac3c50fd160ed16dd6d6053c5d6f58dc6
nanoha <nanoha.creator@gmail.com>
2026-05-16 14:14:58 +0900

    docker-entrypoint.sh更新

--- server/docker/production/docker-entrypoint.sh
+++ server/docker/production/docker-entrypoint.sh
@@ -3,4 +3,10 @@ set -e
 PORT="${PORT:-8080}"
 export PORT
 sed "s/__PORT__/${PORT}/g" /etc/nginx/templates/default.conf > /etc/nginx/conf.d/default.conf
+cd /var/www/html
+php artisan config:cache --no-interaction
+php artisan route:cache --no-interaction
+if [ "$#" -gt 0 ]; then
+  exec "$@"
+fi
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf -n
