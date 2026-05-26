c39b684a0df8c78a7ace9aab1cb135cf1353ee0e
nanoha <nanoha.creator@gmail.com>
2026-05-26 19:52:32 +0900

    画像アップロードできない問題を解消、画像を変更した際に保存ボタンを押下できるようにする

--- client/src/components/handlers/index.ts
+++ client/src/components/handlers/index.ts
@@ -1,4 +1,4 @@
+export { default as DataHandler } from './DataHandler';
 export { default as RedirectHandler } from './RedirectHandler';
 export { default as SnackbarHandler } from './SnackbarHandler';
-export { default as DataHandler } from './DataHandler';
 export { default as VerifiedHandler } from './VerifiedHandler';
