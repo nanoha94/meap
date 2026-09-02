d2f5e93c76ec20602416010de604246638637675
nanoha <nanoha.creator@gmail.com>
2026-09-02 20:45:06 +0900

    ビルドエラーになるので元に戻す

--- server/docker/production/docker-entrypoint.sh
+++ server/docker/production/docker-entrypoint.sh
@@ -12,7 +12,5 @@ if [ ! -f public/index.php ]; then
 fi
 php artisan config:cache --no-interaction
 php artisan route:cache --no-interaction
-chown -R www-data:www-data /var/www/html/bootstrap/cache
-chown -R www-data:www-data /var/lib/nginx /var/log/nginx
 echo "[meap] starting supervisord (nginx + php-fpm)"
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf -n
