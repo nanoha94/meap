bd1ae29e0872ff037ae3ddd7498ec683806777b1
nanoha <nanoha.creator@gmail.com>
2026-09-05 19:46:59 +0900

    メール本文のデザインをアプリデザインに合わせる

---
+++ server/resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ asset('images/meap-logo2.png') }}" class="logo" alt="meap">
+</a>
+</td>
+</tr>
